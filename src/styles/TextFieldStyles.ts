2a733d84eb06cc97d5c6ccecc35cbde803154cea
young ha ryu <ryuyh2000@gamil.com>
2024-06-09 19:42:30 +0900

    design: text field css 전역 선언

---
+++ src/styles/TextFieldStyles.ts
@@ -0,0 +1,11 @@
+import theme from "./theme";
+
+const TextFieldStyle = {
+  focusColor: theme.primary.p100,
+  err: theme.danger.d100,
+  bottomBorderColor: theme.gray.g20,
+  TextFieldDone: theme.gray.g100,
+  disabledBackgroundColor: theme.background,
+};
+
+export const TextFieldStyles = { TextFieldStyle };
