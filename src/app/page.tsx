b49ffd7cfbc786da8a09664f8e80d38ef8d25cb2
sadie100 <03sadie@gmail.com>
2024-08-29 12:30:44 +0900

    fix: 모든 페이지 공통적인 속성을 app/layout으로 옮김

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,8 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export default function Home() {
-  return (
-    <Main>
-      <StyleLink href="/login">로그인 페이지로 이동</StyleLink>
-    </Main>
-  );
+  return <StyleLink href="/login">로그인 페이지로 이동</StyleLink>;
 }
-
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 6rem;
-  min-height: 100vh;
-`;
 
 const StyleLink = styled(Link)`
   background-color: ${({ theme }) => theme.gray.g10};
