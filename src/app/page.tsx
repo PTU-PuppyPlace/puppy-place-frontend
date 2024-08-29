a3cb5efcdb772bf49063546739cfd470b700250c
sadie100 <03sadie@gmail.com>
2024-08-29 12:27:50 +0900

    chore: 사용하지 않는 스타일이 있는 page.module.css 제거하고 styled-component 방식으로 스타일 적용함

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import styles from "./page.module.css";
 import Link from "next/link";
 import styled from "styled-components";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <Main>
       <StyleLink href="/login">로그인 페이지로 이동</StyleLink>
-    </main>
+    </Main>
   );
 }
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6rem;
+  min-height: 100vh;
+`;
 
 const StyleLink = styled(Link)`
   background-color: ${({ theme }) => theme.gray.g10};
