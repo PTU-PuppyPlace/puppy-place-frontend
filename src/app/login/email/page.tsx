a407ada4023709ecf6d92d5debc0d1a4df67aa67
sadie100 <03sadie@gmail.com>
2024-08-30 13:02:59 +0900

    feat: 로그인페이지 ui 뼈대 구현

--- src/app/login/email/page.tsx
+++ src/app/login/email/page.tsx
@@ -3,31 +3,32 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import TextField from "@/components/common/TextField";
+import { Checkbox } from "@/components/common/CheckControl";
+import Button from "@/components/common/Button";
 
 const Login = () => {
-  return <div>로그임</div>;
+  return (
+    <Section>
+      <TextField placeholder="아이디(이메일)" onChange={() => {}} value="" />
+      <TextField placeholder="비밀번호" onChange={() => {}} value="" />
+      <Checkbox checked={false} onChange={() => {}}>
+        자동로그인
+      </Checkbox>
+      <Button onClick={() => {}} variant="primary" size="52">
+        로그인
+      </Button>
+      <Link href="/login/find-password">비밀번호 찾기</Link>
+      <Link href="/login/signup">회원가입</Link>
+    </Section>
+  );
 };
 
 export default Login;
 
-const Section = styled.div`
+const Section = styled.form`
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const ButtonSection = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
   gap: 16px;
-`;
-
-const StyledLink = styled(Link)`
-  color: ${({ theme }) => theme.gray.g100};
-  font-size: ${({ theme }) => theme.body14};
-  font-weight: ${({ theme }) => theme.medium};
-  cursor: pointer;
 `;
