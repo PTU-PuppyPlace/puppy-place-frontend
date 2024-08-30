861ceb1cf78ce8217fbce7592a02aee274ca2614
sadie100 <03sadie@gmail.com>
2024-08-30 12:49:50 +0900

    feat: 이메일로 시작하기 페이지 ui 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import loginLogo from "./login-logo.png";
+import kakaoButton from "./kakao-button.png";
+import appleButton from "./apple-button.png";
+import googleButton from "./google-button.png";
+import styled from "styled-components";
+import Spacer from "@/components/common/Spacer";
+import Link from "next/link";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <Section>
+      <Image src={loginLogo} alt="login logo" />
+      <Spacer height={"60px"} />
+      <ButtonSection>
+        <Image src={kakaoButton} alt="kakao login button" />
+        <Image src={appleButton} alt="apple login button" />
+        <Image src={googleButton} alt="google login button" />
+      </ButtonSection>
+      <Spacer height={"24px"} />
+      <StyledLink href="/login/email">이메일로 시작하기</StyledLink>
+    </Section>
+  );
 };
 
 export default Login;
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ButtonSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const StyledLink = styled(Link)`
+  color: ${({ theme }) => theme.gray.g100};
+  font-size: ${({ theme }) => theme.body14};
+  font-weight: ${({ theme }) => theme.medium};
+  cursor: pointer;
+`;
---
+++ src/components/common/Spacer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function Spacer({
+  width,
+  height,
+}: {
+  width?: string;
+  height?: string;
+}) {
+  return <StyledSpacer width={width} height={height} />;
+}
+
+const StyledSpacer = styled.div<{ width?: string; height?: string }>`
+  width: ${({ width }) => (width ? width : "100%")};
+  height: ${({ height }) => (height ? height : "100%")};
+`;
