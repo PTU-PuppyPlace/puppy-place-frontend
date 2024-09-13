ed615f1884cabad249434a76c7942952614c0eb5
sadie100 <03sadie@gmail.com>
2024-09-13 17:40:22 +0900

    feat: 이메일로 시작하기 페이지 UI 구현

--- src/app/login/email/page.tsx
+++ src/app/login/email/page.tsx
@@ -14,18 +14,22 @@ const Login = () => {
   );
 
   return (
-    <Section>
-      <TextField placeholder='아이디(이메일)' onChange={() => {}} value='' />
-      <TextField placeholder='비밀번호' onChange={() => {}} value='' />
-      <Checkbox checked={isChecked} onChange={handleCheck}>
-        자동로그인
-      </Checkbox>
-      <Button onClick={() => {}} variant='primary' size='52'>
-        로그인
-      </Button>
-      <Link href='/login/find-password'>비밀번호 찾기</Link>
-      <Link href='/login/signup'>회원가입</Link>
-    </Section>
+    <>
+      <Section>
+        <TextField placeholder='아이디(이메일)' onChange={() => {}} value='' />
+        <TextField placeholder='비밀번호' onChange={() => {}} value='' />
+        <Checkbox checked={isChecked} onChange={handleCheck}>
+          자동로그인
+        </Checkbox>
+        <Button onClick={() => {}} variant='primary' size='52'>
+          로그인
+        </Button>
+      </Section>
+      <Links>
+        <Link href='/login/signup'>회원가입</Link>
+        <Link href='/login/find-password'>비밀번호 재설정</Link>
+      </Links>
+    </>
   );
 };
 
@@ -36,4 +40,16 @@ const Section = styled.form`
   justify-content: center;
   flex-direction: column;
   gap: 16px;
+`;
+
+const Links = styled.div`
+  margin-top: 24px;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.gray.g60};
+  gap: 8px;
+  > *:not(:last-child)::after {
+    content: '|';
+    margin-left: 8px;
+  }
 `;
