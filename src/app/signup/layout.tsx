f8040318aaca0a9afd36ff06a1180462ac0eec48
sadie100 <03sadie@gmail.com>
2024-09-13 18:17:33 +0900

    feat: 회원가입 페이지 생성, 구현 중

---
+++ src/app/signup/layout.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Header from '@/components/layout/Header';
+import styled from 'styled-components';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header
+        onGoBack={() => {
+          console.log('뒤로가기');
+        }}
+      >
+        회원가입
+      </Header>
+      <Section>{children}</Section>
+    </>
+  );
+}
+
+const Section = styled.div`
+  margin-top: 66px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 32px;
+  width: 100%;
+  border: 1px solid blue;
+`;
