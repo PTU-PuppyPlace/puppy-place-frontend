42c5cff48695c28020e59973698832de89fbdb1c
sadie100 <03sadie@gmail.com>
2024-09-13 13:26:25 +0900

    feat: 페이지 코드 포맷팅 변경 적용, CheckBox value 픽스했던 거 품

--- src/app/login/email/page.tsx
+++ src/app/login/email/page.tsx
@@ -1,25 +1,30 @@
-"use client";
+'use client';
 
-import React from "react";
-import styled from "styled-components";
-import Link from "next/link";
-import TextField from "@/components/common/TextField";
-import { Checkbox } from "@/components/common/CheckControl";
-import Button from "@/components/common/Button";
+import React from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+import TextField from '@/components/common/TextField';
+import { Checkbox } from '@/components/common/CheckControl';
+import Button from '@/components/common/Button';
 
 const Login = () => {
+  const [isChecked, handleCheck] = React.useReducer(
+    (checked) => !checked,
+    false
+  );
+
   return (
     <Section>
-      <TextField placeholder="아이디(이메일)" onChange={() => {}} value="" />
-      <TextField placeholder="비밀번호" onChange={() => {}} value="" />
-      <Checkbox checked={false} onChange={() => {}}>
+      <TextField placeholder='아이디(이메일)' onChange={() => {}} value='' />
+      <TextField placeholder='비밀번호' onChange={() => {}} value='' />
+      <Checkbox checked={isChecked} onChange={handleCheck}>
         자동로그인
       </Checkbox>
-      <Button onClick={() => {}} variant="primary" size="52">
+      <Button onClick={() => {}} variant='primary' size='52'>
         로그인
       </Button>
-      <Link href="/login/find-password">비밀번호 찾기</Link>
-      <Link href="/login/signup">회원가입</Link>
+      <Link href='/login/find-password'>비밀번호 찾기</Link>
+      <Link href='/login/signup'>회원가입</Link>
     </Section>
   );
 };
