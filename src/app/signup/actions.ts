10985394d8df6ab502e21111c3b2ae6f761df941
sadie100 <03sadie@gmail.com>
2025-01-19 23:00:49 +0900

    feat: validation을 위한 스키마 및 action 파일을 분리해서 작성

---
+++ src/app/signup/actions.ts
@@ -0,0 +1,38 @@
+'use server';
+
+import { SignupSchemaType } from './schema';
+
+type SignupState = {
+  errors?: {
+    username?: string;
+    password?: string;
+    passwordConfirm?: string;
+    nickname?: string;
+    agreement?: string;
+    isMarketingAgreed?: string;
+    isEmailVerified?: string;
+  };
+  message?: string;
+};
+
+export type EmailVerificationStatus = {
+  showVerification: boolean;
+  isVerified: boolean;
+};
+
+export async function signup(data: SignupSchemaType): Promise<SignupState> {
+  console.log('signup action');
+
+  try {
+    console.log(data);
+    return {
+      message: '회원가입 성공',
+    };
+  } catch {
+    return {
+      message: '회원가입 중 오류가 발생했습니다. 다시 시도해주세요.',
+    };
+  }
+}
+
+export async function sendEmailCode() {}
