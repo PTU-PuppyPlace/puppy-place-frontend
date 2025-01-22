49378c15b37876ed6753a4695fc901ccb534e371
sadie100 <03sadie@gmail.com>
2025-01-22 21:59:19 +0900

    feat: account api 코드 작성, signup action에 이메일 확인 로직 작성

---
+++ src/services/api/account.ts
@@ -0,0 +1,44 @@
+import { apiClient } from './apiClient';
+
+export async function signup(data: any) {
+  const response = await apiClient.post(
+    '/account/member/register/process',
+    data
+  );
+  return response;
+}
+
+export async function sendEmailCode(email: string) {
+  const response = await apiClient.post('/account/verification/create', {
+    email,
+  });
+  return response;
+}
+
+export async function checkEmailCode(email: string, authCode: string) {
+  const response = await apiClient.post('/account/verification/confirm', {
+    email,
+    authCode,
+  });
+  return response;
+}
+
+// 닉네임 중복 확인
+export async function checkNickname(nickname: string) {
+  const response = await apiClient.post(
+    '/account/member/register/check/nickname',
+    {
+      nickname,
+    }
+  );
+  return response;
+}
+
+// 회원가입 전 사용자의 이름, 전화번호를 확인하여 회원가입한 사용자인지 확인
+export async function checkUser(username: string, authCode: string) {
+  const response = await apiClient.post('/account/member/register/init', {
+    username,
+    authCode,
+  });
+  return response;
+}
