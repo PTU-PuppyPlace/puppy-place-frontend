594ac0ccb0026c68d952941ba5c6831ea25e7d45
sadie100 <03sadie@gmail.com>
2025-02-01 18:37:44 +0900

    chore: /sample의 implicit any 타입오류 수정

--- src/app/sample/actions.ts
+++ src/app/sample/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
-export async function componentTestAction(currentState, formData: FormData) {
+export async function componentTestAction(
+  currentState: any,
+  formData: FormData
+) {
   const data = Object.fromEntries(formData);
   console.log('data', data);
   return {
