41d47400d67faaaf3d64de1235b3b0e31260049e
sadie100 <03sadie@gmail.com>
2025-02-23 17:53:50 +0900

    feat: 탭바 레이아웃 대충 구현

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Button from '@/components/common/Button';
-import BottomTabBar from '@/components/layout/Tabbar';
+import BottomTabBar from '@/components/layout/BottomTabbar';
 import { logout } from '@/services/account';
 
 export default function Map() {
